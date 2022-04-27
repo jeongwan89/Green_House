e4c8fe9db197fc367129e3ec08cdf5019bf20452
jeongwan89 <jeongwan89@mac.com>
2022-04-27 16:34:49 +0900

    엄청난 에러 디버깅 (Blynk.virtualSync -> .synkAll 바꿨음)

--- ProMini_with_Esp01Modem/Blynk_for_GH_Mist/Blynk_for_GH_Mist.ino
+++ ProMini_with_Esp01Modem/Blynk_for_GH_Mist/Blynk_for_GH_Mist.ino
@@ -1,36 +1,48 @@
 /*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  This example shows how to use ESP8266 Shield (with AT commands)
+  to connect your project to Blynk.
+
   WARNING!
     It's very tricky to get it working. Please read this article:
     http://help.blynk.cc/hardware-and-libraries/arduino/esp8266-with-at-firmware
 
-  You’ll need:
-   - Blynk IoT app (download from App Store or Google Play)
-   - Arduino Uno board
-   - Decide how to connect to Blynk
-     (USB, Ethernet, Wi-Fi, Bluetooth, ...)
-
-  There is a bunch of great example sketches included to show you how to get
-  started. Think of them as LEGO bricks  and combine them as you wish.
-  For example, take the Ethernet Shield sketch and combine it with the
-  Servo example, or choose a USB sketch and add a code from SendData
-  example.
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
  *************************************************************/
 
-// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
-// See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID           "TMPLxxxxxx"
-#define BLYNK_DEVICE_NAME           "Device"
-#define BLYNK_AUTH_TOKEN            "zM3g35YO8NtLM5S03VYnMLkRd3BNynQZ"
 
-
-// Comment this out to disable prints and save space
-#define BLYNK_PRINT Serial
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+//#define BLYNK_TEMPLATE_ID   "YourTemplateID"
 
 
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
 
-char auth[] = BLYNK_AUTH_TOKEN;
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "zM3g35YO8NtLM5S03VYnMLkRd3BNynQZ";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
@@ -51,36 +63,30 @@ ESP8266 wifi(&EspSerial);
 
 void setup()
 {
+  pinMode(4, OUTPUT);
   // Debug console
   Serial.begin(115200);
 
+  delay(10);
+
   // Set ESP8266 baud rate
   EspSerial.begin(ESP8266_BAUD);
-  delay(50);
+  delay(10);
 
-  //Blynk.begin(auth, wifi, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(auth, wifi, ssid, pass, "cloud.blynk.cc", 80);
-  Blynk.begin(auth, wifi, ssid, pass, "blynk-cloud.com");
-  //Blynk.begin(auth, wifi, ssid, pass, IPAddress(192,168,1,100), 8080);
-  pinMode(4,OUTPUT);
-  Blynk.syncVirtual();
+  Blynk.begin(auth, wifi, ssid, pass);
 }
 
 void loop()
 {
   Blynk.run();
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
 }
 
 BLYNK_WRITE(V2) //펌프기동 스위치
 {
     int value = param.asInt(); // Get value as integer
     //debug code
-    /*Serial.print("State of V2 = ");
-    Serial.println(value);*/
+    //Serial.print("State of V2 = ");
+    //Serial.println(value);
     if(value == 1) {
       digitalWrite(4, HIGH);
     }
@@ -88,14 +94,16 @@ BLYNK_WRITE(V2) //펌프기동 스위치
       digitalWrite(4, LOW);
     }
 }
+
+
 BLYNK_WRITE(V0) //펌프기동 타이머
 {
   int value = param.asInt();
-  if(value = 1) {
+  if(value == 1) {
     Blynk.virtualWrite(V2, 1);
   }
   else {
     Blynk.virtualWrite(V2, 0);
   }
-  Blynk.syncVirtual();
+  Blynk.syncAll();
 }
