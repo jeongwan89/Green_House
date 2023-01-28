b52883d2cac1a9f6cace22cedf83853177c9f89c
jeongwan89 <jeongwan89@mac.com>
2023-01-28 22:42:45 +0900

    약간의 optimize가 이루어진 코드이다. 이번에 특이할 것은 sys.setrecu...

--- Monitor_ThingSpeak/ttkbootstrap_modified_kim.py
+++ Monitor_ThingSpeak/ttkbootstrap_modified_kim.py
@@ -1,3 +1,4 @@
+import sys
 import urllib.request
 import json
 from pprint import pprint
@@ -5,40 +6,53 @@ from pprint import pprint
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
+sys.setrecursionlimit = 2500
 
 GAP_SEC = 15000 # time delay in millisecond
 app = ttk.Window()
+
+MY_URL = ["https://api.thingspeak.com/channels/846408/feeds.json?results=1&timezone=Asia%2FSeoul",
+            "https://api.thingspeak.com/channels/908037/feeds.json?results=1&timezone=Asia%2FSeoul",
+            "https://api.thingspeak.com/channels/1468404/feeds.json?results=1&timezone=Asia%2FSeoul",
+            "https://api.thingspeak.com/channels/1468405/feeds.json?results=1&timezone=Asia%2FSeoul"]
+
+GREEN_HOUSE = []
+for my_url_str in MY_URL:
+    TS = urllib.request.urlopen(my_url_str)
+    GREEN_HOUSE.append(json.loads(TS.read()))
+    pprint(GREEN_HOUSE[-1])
+    a = GREEN_HOUSE[-1]['channel']['name']
+    b = GREEN_HOUSE[-1]['channel']['field3']
+    c = GREEN_HOUSE[-1]['feeds'][-1]['field3']
+    print (a + " : " + b + " : " + c)
+    TS.close()
 
 def read_thing_speak() :
     '''
     온실에서 ThinsSpeak 서버에 게시한 data를 읽는 함수
     주의: 무한 루프에서 실행될 때, 매번 지속적으로 이 함수가 실행되면, 서버에서 블록킹 되어 버린다.
         내 단말기에서 올리는 시간은 15초마다 한번씩이니까 여기에 동조해 설정하면 많은 문제가 해결된다.
-    리턴값으로 읽은 json 데이터를 돌려보핸다.
-    return:
-        각 온실의 json값을 list로 모아서 보낸다.
+    인수:
+        없다.
+    리턴:
+        없다.
+    인수와 리턴값이 없는 이유는 global GREEN_HOUSE[]안에 바로 쓰고 읽고 하기 때문이다.
+    
     '''
-    my_url = ["https://api.thingspeak.com/channels/846408/feeds.json?results=1&timezone=Asia%2FSeoul",
-              "https://api.thingspeak.com/channels/908037/feeds.json?results=1&timezone=Asia%2FSeoul",
-              "https://api.thingspeak.com/channels/1468404/feeds.json?results=1&timezone=Asia%2FSeoul",
-              "https://api.thingspeak.com/channels/1468405/feeds.json?results=1&timezone=Asia%2FSeoul"]
-    response = []
-    data = []
+    global MY_URL
+    global GREEN_HOUSE
     i = 0
 
-    for my_url_str in my_url:
+    for my_url_str in MY_URL:
         TS = urllib.request.urlopen(my_url_str)
-        response.append(TS.read())
-        data.append(json.loads(response[i]))
-        pprint(data[i])
-        a = data[i]['channel']['name']
-        b = data[i]['channel']['field3']
-        c = data[i]['feeds'][-1]['field3']
+        GREEN_HOUSE[i] = json.loads(TS.read())
+        pprint(GREEN_HOUSE[i])
+        a = GREEN_HOUSE[i]['channel']['name']
+        b = GREEN_HOUSE[i]['channel']['field3']
+        c = GREEN_HOUSE[i]['feeds'][-1]['field3']
         print (a + " : " + b + " : " + c)
         TS.close()
         i = i + 1
-    
-    return data
 
 def meter_change(*args):
     '''
@@ -67,7 +81,6 @@ def meter_change(*args):
         else :
             m['bootstyle'] = DARK
 
-GREEN_HOUSE = []
 
 def update_state():
     '''
@@ -77,45 +90,33 @@ def update_state():
     global GREEN_HOUSE
     global METER_GAUGE
 
-    GREEN_HOUSE = read_thing_speak()
+    read_thing_speak()
 
     for i in range(0,4) :
         METER_GAUGE[i]['amountused'] = GREEN_HOUSE[i]['feeds'][-1]['field3']
 
+    app.after(GAP_SEC, update_state)
 
-METER_GAUGE = [] 
+
+METER_GAUGE = []
 #meter1~4 초기화한다.
 for i in range(0,4):
-    label = "%d동 온도" %i
-    METER_GAUGE.append(
-                    ttk.Meter(app, metersize=180, padding=20, amounttotal=70, 
-                              arcrange=270, arcoffset=-180, amountused=0, textleft='°C',
-                              subtext=label, wedgesize=10, bootstyle='default', interactive=True)
+    label = "%d동 온도" %(i+1)
+    METER_GAUGE.append(ttk.Meter(app, metersize=180, padding=20, amounttotal=70, 
+                                 arcrange=270, arcoffset=-180, amountused=0, textleft='°C',
+                                 subtext=label, wedgesize=10, bootstyle='default', interactive=True)
                       ) 
 
-METER_GAUGE = [] 
-#meter1~4 초기화한다.
-for i in range(0,4):
-    label = "%d동 온도" %i
-    METER_GAUGE.append(
-                    ttk.Meter(app, metersize=180, padding=20, amounttotal=70, 
-                              arcrange=270, arcoffset=-180, amountused=0, textleft='°C',
-                              subtext=label, wedgesize=10, bootstyle='default', interactive=True)
-                      ) 
-
-METER_GAUGE[0].grid(row=0, column=0)
-METER_GAUGE[1].grid(row=0, column=1)
-METER_GAUGE[2].grid(row=1, column=0)
-METER_GAUGE[3].grid(row=1, column=1)
+for i in range(0, 4):
+    METER_GAUGE[i].grid(row=0, column=i)
 
 
 for m in METER_GAUGE:
     m.amountusedvar.trace("w", meter_change)
 
 #meter2.amountusedvar.trace("w", meter_change)
-initial_data = []
-initial_data = read_thing_speak()
+
 for i in range(0,4):
-    METER_GAUGE[i].configure(amountused = initial_data[i]['feeds'][-1]['field3'])
+    METER_GAUGE[i].configure(amountused = GREEN_HOUSE[i]['feeds'][-1]['field3'])
 app.after(GAP_SEC, update_state)
 app.mainloop()
