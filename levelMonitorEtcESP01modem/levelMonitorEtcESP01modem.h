174db466199244c07c77eb27274ee8470c2facf3
jeongwan89 <jeongwan89@mac.com>
2023-05-12 22:51:37 +0900

    일단 세이프

--- levelMonitorEtcESP01modem/levelMonitorEtcESP01modem.h
+++ levelMonitorEtcESP01modem/levelMonitorEtcESP01modem.h
@@ -19,6 +19,67 @@ PubSubClient client(espClient);
 #define MQTTID "levelMonitor"
 #define MQTTUSER "farmmain"
 #define MQTTPASS "eerrtt"
+#define WILLTOPIC "Lastwill/FM/fertank/status"
+#define WILLMSG "off line"
 
 #define TRIG 2
 #define ECHO 3
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    char str[256];
+    int conv;
+    Serial.print("Message arrived []");
+    Serial.print(topic);
+    Serial.print("]");
+    for(int i = 0; i < length; i++){
+        Serial.print((char) payload[i]);
+        str[i] = (char) payload[i];
+        str[i+1] = NULL;
+    }
+    // 이하 특별한 topic message에 대한 처리 루틴이 있어야한다.
+}
+
+void reconnect(void) {
+    while(!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        if(client.connect(MQTTID,MQTTUSER,MQTTPASS,WILLTOPIC, 0, 1, WILLMSG)) {
+            Serial.print("connected");
+            client.publish(WILLTOPIC, "on line");
+            // client.subscribe("...");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.print("\t try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+
+void wifiConnect(void) 
+{
+/* setup() 안에서 작동한다. 이전에 반드시 정의 되어야할 것이 있는데, 
+    ESP01이 SoftwareSerial로 정의되어 있어야 하고(EspSerial),
+    WiFi 쉴드 변수와 ESP01모뎀의 객체의 레퍼런스를 연결해 주어야 한다.
+*/
+    EspSerial.begin(ESP8266_BAUD);
+    WiFi.init(&EspSerial);
+
+    if(WiFi.status() == WL_NO_SHIELD) {
+        Serial.println("WiFi shield not present");
+        while(true);
+    }
+
+    Serial.print("Attempting to connect to WPA SSID:");
+    Serial.println(ssid);
+    
+    WiFi.begin(ssid, pass);
+
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("Your're connected to the networtk");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
--- levelMonitorEtcESP01modem/levelMonitorEtcESP01modem.ino
+++ levelMonitorEtcESP01modem/levelMonitorEtcESP01modem.ino
@@ -1,2 +1,33 @@
 #include "levelMonitorEtcESP01modem.h"
 
+void setup(void){
+    Serial.begin(115200);
+
+    wifiConnect();
+
+    client.setServer(server, 1883);
+    client.setCallback(callback);
+}
+
+void loop(void){
+//파트1
+    status = WiFi.status();
+    if ( status != WL_CONNECTED) {
+        while ( status != WL_CONNECTED) {
+            Serial.print("Attempting to connect to WPA SSID: ");
+            Serial.println(ssid);
+            // Connect to WPA/WPA2 network
+            status = WiFi.begin(ssid, pass);
+            delay(500);
+        }
+        Serial.println("Connected to AP");
+        IPAddress ip = WiFi.localIP();
+  
+        Serial.print("IP Address: ");
+        Serial.println(ip);
+    }
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+}
