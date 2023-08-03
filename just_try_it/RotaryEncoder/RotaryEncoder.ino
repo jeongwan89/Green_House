7a4c4e7389c089aafc2c37bcdd545c51fefb237d
jeongwan89 <jeongwan89@mac.com>
2023-08-03 23:17:13 +0900

    Add try it directory

---
+++ just_try_it/RotaryEncoder/RotaryEncoder.ino
@@ -0,0 +1,57 @@
+#define CLK 2    // 2번핀을 CLK에 연결
+#define DT 3     // 3번핀을 DT에 연결
+
+int counter = 0;   // 카운팅 저장용 변수
+int currentStateCLK;     // 현재 CLK의 상태 저장용 변수
+int lastStateCLK;         // 이전 CLK의 상태 저장용 변수
+String currentDir ="";     // 현재 방향 출력용 문자열 변수
+
+void setup() {
+	
+	// 엔코더 핀을 입력으로 설정
+	pinMode(CLK,INPUT);
+	pinMode(DT,INPUT);
+
+	// 시리얼 출력 개시
+	Serial.begin(9600);
+
+	// CLK핀의 최초 상태 저장
+	lastStateCLK = digitalRead(CLK);
+	
+	//외부 인터럽트 등록, 핀의 상태가 변할 때(HIGH에서 LOW 또는 LOW에서 HIGH) 마다 updateEncoder함수가 실행됨. 
+	// 인터럽트 0번은 2번핀과 연결되어 있고 1번은 3번 핀과 연결되어 있음
+	attachInterrupt(0, updateEncoder, CHANGE);
+	attachInterrupt(1, updateEncoder, CHANGE);
+}
+
+void loop() {
+	//loop문에서는 따로 실행할 코드가 없음.
+}
+
+void updateEncoder(){   // 인터럽트 발생시 실행되는 함수
+	// CLK의 현재 상태를 읽어서 
+	currentStateCLK = digitalRead(CLK);
+
+	// CLK핀의 신호가 바뀌었고(즉, 로터리엔코더의 회전이 발생했했고), 그 상태가 HIGH이면(최소 회전단위의 회전이 발생했다면) 
+	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+
+		// DT핀의 신호를 확인해서 엔코더의 회전 방향을 확인함.
+		if (digitalRead(DT) != currentStateCLK) {    // 신호가 다르다면 시계방향 회전
+			counter ++;                                // 카운팅 용 숫자 1 증가
+			currentDir ="시계방향 회전";
+		} else {                                   // 신호가 같다면 반시계방향 회전
+			counter --;                              // 카운팅 용 숫자 1 감소
+			currentDir ="반시계 방향 회전";
+		}
+
+
+
+		Serial.print("회전방향: ");
+		Serial.print(currentDir);               // 회전방향 출력
+		Serial.print(" | Counter: ");
+		Serial.println(counter);              // 카운팅 출력
+	}
+
+	// 마지막 상태 변수 저장
+	lastStateCLK = currentStateCLK;
+}
