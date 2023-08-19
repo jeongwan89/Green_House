6e9af7db1464af477ed3038f24647acfc5fe7128
jeongwan89 <jeongwan89@mac.com>
2023-08-19 16:19:03 +0900

    7segments driver

--- ProMini_encoder_timer/ProMini_encoder_timer.ino
+++ ProMini_encoder_timer/ProMini_encoder_timer.ino
@@ -5,6 +5,10 @@
 #define DT 2 	// 3번핀을 DT에 연결 S2
 #define SW 4	// incremental encoder의 누름 스위치. 누르면 LOW
 #define BUILTIN_LED 13
+// 8 digits 7 segments LED drived by max7219
+#define DIN7219 12	
+#define CLK7219 11
+#define CS7219	10
 int counter = 0;   // 카운팅 저장용 변수
 int currentStateCLK;     // 현재 CLK의 상태 저장용 변수
 int lastStateCLK;         // 이전 CLK의 상태 저장용 변수
@@ -12,7 +16,17 @@ String currentDir ="";     // 현재 방향 출력용 문자열 변수
 
 
 // 4 digits 7 segments LED display driver part
-// [] 여기에 코드를 넣어야 한다.
+byte adr = 0x08;
+byte num = 0x00;
+int i = 0;
+
+void shift(byte send_to_address, byte send_this_data)
+{
+  digitalWrite(CS7219, LOW);
+  shiftOut(DIN7219, CLK7219, MSBFIRST, send_to_address);
+  shiftOut(DIN7219, CLK7219, MSBFIRST, send_this_data);
+  digitalWrite(CS7219, HIGH);
+}
 
 // 이 함수가 호출되면 다이얼 counter을 0으로 reset
 void resetCounter(){
@@ -27,6 +41,20 @@ void setup() {
 	pinMode(DT, INPUT);
 	pinMode(SW, INPUT);
 
+	//8 digits 7 segments LED of max7219
+	pinMode(DIN7219, OUTPUT);
+	pinMode(CS7219, OUTPUT);
+	pinMode(CLK7219, OUTPUT);
+	digitalWrite(CLK7219, LOW);
+	delay(200);
+
+	//Setup of MAX7219 chip
+	shift(0x0f, 0x00); //display test register - test mode off
+	shift(0x0c, 0x01); //shutdown register - normal operation
+	shift(0x0b, 0x07); //scan limit register - display digits 0 thru 7
+	shift(0x0a, 0x0f); //intensity register - max brightness
+  	shift(0x09, 0xff); //decode mode register - CodeB decode all digits
+	
 	// 시리얼 출력 개시
 	Serial.begin(115200);
 
@@ -76,6 +104,27 @@ void loop() {
 	}
 
 	prevSwitchStat = switchStat;
+
+	// 8 digits 7 segments LED of max7219
+	//Data transfer
+	shift(0x08, 0x00); //digit 7 (leftmost digit) data
+	shift(0x07, 0x01);
+	shift(0x06, 0x02);
+	shift(0x05, 0x03);
+	shift(0x04, 0x04);
+	shift(0x03, 0x05);
+	shift(0x02, 0x06);
+	shift(0x01, 0x07); //digit 0 (rightmost digit) data
+	delay(1000);
+	shift(0x08, 0xf7); //digit 7 (leftmost digit) data
+	shift(0x07, 0xf6);
+	shift(0x06, 0xf5);
+	shift(0x05, 0xf4);
+	shift(0x04, 0xf3);
+	shift(0x03, 0xf2);
+	shift(0x02, 0xf1);
+	shift(0x01, 0xf0); //digit 0 (rightmost digit) data
+	delay(1000);
 
 }
 
