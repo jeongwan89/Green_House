31581ced1cb4c6e6f4af3319917264174f629e3f
jeongwan89 <jeongwan89@mac.com>
2023-08-20 11:58:08 +0900

    7segments LED unit test finished

--- ProMini_encoder_timer/ProMini_encoder_timer.ino
+++ ProMini_encoder_timer/ProMini_encoder_timer.ino
@@ -5,7 +5,7 @@
 #define CLK 3 	// 2번핀을 CLK에 연결 S1
 #define DT 2 	// 3번핀을 DT에 연결 S2
 #define SW 4	// incremental encoder의 누름 스위치. 누르면 LOW
-#define BUILTIN_LED 13
+#define RELAY 13
 // 8 digits 7 segments LED drived by max7219
 #define DIN7219 12	
 #define CLK7219 11
@@ -55,15 +55,17 @@ void PrintSeg(int counter){
 
 // nCounter를 인수로 받아서 60진법 표시로 바꾸어 보낸다.
 // mm-ss-ds 형태로 보낸다.
-void PrintTimeSeg(int nCounter){
+void PrintTimeSeg(double fCounter){
 	//nCounter의 범위는 0~3599가 의미 있는 수 이고 이것 밖에 있는 것은 취급하지 않는 것으로 한다.
-	if(nCounter > 3600 || nCounter < 0){
+	/*if(fCounter > 3600 || fCounter < 0){
 		SetZero();
 		return;
 	}
-	int sMin, sSec;
-	sMin = nCounter / 60;
-	sSec = nCounter % 60;
+	*/
+	unsigned long sMin, sSec, sMilli;
+	sMin = (unsigned long) fCounter / 60;
+	sSec = (unsigned long) fCounter % 60;
+	sMilli = (fCounter - (unsigned long)fCounter) * 100;
 
 	shift(8, sMin/10);
 	shift(7, sMin%10);
@@ -71,8 +73,8 @@ void PrintTimeSeg(int nCounter){
 	shift(5, sSec/10);
 	shift(4, sSec%10);
 	shift(3, 10);
-	shift(2, 0);
-	shift(1, 0);
+	shift(2, sMilli/10);
+	shift(1, sMilli%10);
 }
 // 이 함수가 호출되면 다이얼 counter을 0으로 reset
 void resetCounter(){
@@ -82,8 +84,37 @@ void resetCounter(){
 	SetZero();
 }
 
-void setup() {
+// 인터럽트 발생시 실행되는 함수
+void updateEncoder(){   
 
+	// CLK의 현재 상태를 읽어서 
+	currentStateCLK = digitalRead(CLK);
+
+	// CLK핀의 신호가 바뀌었고(즉, 로터리엔코더의 회전이 발생했했고), 그 상태가 HIGH이면(최소 회전단위의 회전이 발생했다면) 
+	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+		// DT핀의 신호를 확인해서 엔코더의 회전 방향을 확인함.
+		if (digitalRead(DT) != currentStateCLK) {    // 신호가 다르다면 시계방향 회전
+			counter ++;                                // 카운팅 용 숫자 1 증가
+			currentDir ="CW";
+		} else {                                   // 신호가 같다면 반시계방향 회전
+			counter --;                              // 카운팅 용 숫자 1 감소
+			currentDir ="CCW";
+		}
+		Serial.print("revolution: ");
+		Serial.println(currentDir);               // 회전방향 출력
+		Serial.print("Counter: ");
+		Serial.println(counter);              // 카운팅 출력
+
+		//7seg에 출력
+		PrintTimeSeg(counter);
+
+	}
+
+	// 마지막 상태 변수 저장
+	lastStateCLK = currentStateCLK;
+}
+
+void setup() {
 	// 엔코더 핀을 입력으로 설정
 	pinMode(CLK, INPUT);
 	pinMode(DT, INPUT);
@@ -122,9 +153,17 @@ void loop() {
 	static int switchStat, prevSwitchStat=1;	// loop()안에서 두 변수의 상태가 할당된다. prevSwitchStat는 loop()떠나기 직전에 switchStat의 값으로 할당된다.
 	static bool LEDstat = false; 				// LEDstat는 바로 점등상태로 연결된다
 	static unsigned long tPressed, tReleased;	// 다이얼 버튼이 눌러진 시간, 헤제된 시간 : 이를 결정하기 위해 조건이 필요하다
+	static unsigned long tPrevPressed = millis();			// doubleclick하기 위한 code
+	static unsigned long tStart; //timer 시작 시간
+	static double fTimer; //PrintTimeSeg에 인자를 넘겨주기 위한 변수
+
 	switchStat = digitalRead(SW);
 	if(switchStat == LOW && prevSwitchStat == HIGH){ // PRESSED
 		LEDstat = !LEDstat;
+		if(LEDstat == true){
+			tStart = millis();
+		}
+		tPrevPressed = tPressed;
 		tPressed = millis();
 		tReleased = millis();
 		Serial.print("tPressed = "); Serial.println(tPressed);
@@ -140,52 +179,44 @@ void loop() {
 		Serial.print("tReleased = "); Serial.println(tReleased); 
 		delay(10);
 	}
+
 	if(LEDstat == true) {
-		digitalWrite(BUILTIN_LED, HIGH);
+		// count down 처리해야한다. *********** 카 운 트 다 운 ****************
+		digitalWrite(RELAY, HIGH);
+		if(millis()-tStart < (unsigned long) counter * 1000) {
+			// Relay가 켜졌고, 눌러졌을 때의 시각이 저장된 카운터보다 작으면, 카운터 작동을 한다
+			fTimer = (double)( (unsigned long) counter * 1000 - (millis() - tStart) ) / 1000;
+			PrintTimeSeg(fTimer);
+		}
+		else{
+			// 릴레이가 켜졌고, 카운터가 다 되었을 때의 작동법
+			PrintTimeSeg(0);
+			counter = 0;
+			LEDstat = false;
+			digitalWrite(RELAY, LOW);
+		}
+		// count down 처리해야한다. *********** 카 운 트 다 운 ****************
 	} else {
-		digitalWrite(BUILTIN_LED, LOW);
+		digitalWrite(RELAY, LOW);
 	}
 
 	if ((tReleased - tPressed) > (unsigned long) 1000){
+		// 누른 시간이 1초 초과이면 리셋
 		Serial.print("tReleased - tPressed = "); Serial.println((tReleased - tPressed));
 		resetCounter();
 		LEDstat = false;
 		tPressed = millis();
 		tReleased = millis();
-	}
+	} 
 
+	// doubleClick되었을 때 counter를 다시 90으로 하고 표시를 해준다.
+	if((tPressed - tPrevPressed) <= (unsigned long) 700){
+		counter = 90;
+		PrintTimeSeg(counter);
+		tPrevPressed = millis();
+	}
 	prevSwitchStat = switchStat;
 
 	// 8 digits 7 segments LED of max7219
 	//Data transfer
-}
-
-// 인터럽트 발생시 실행되는 함수
-void updateEncoder(){   
-
-	// CLK의 현재 상태를 읽어서 
-	currentStateCLK = digitalRead(CLK);
-
-	// CLK핀의 신호가 바뀌었고(즉, 로터리엔코더의 회전이 발생했했고), 그 상태가 HIGH이면(최소 회전단위의 회전이 발생했다면) 
-	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
-		// DT핀의 신호를 확인해서 엔코더의 회전 방향을 확인함.
-		if (digitalRead(DT) != currentStateCLK) {    // 신호가 다르다면 시계방향 회전
-			counter ++;                                // 카운팅 용 숫자 1 증가
-			currentDir ="CW";
-		} else {                                   // 신호가 같다면 반시계방향 회전
-			counter --;                              // 카운팅 용 숫자 1 감소
-			currentDir ="CCW";
-		}
-		Serial.print("revolution: ");
-		Serial.println(currentDir);               // 회전방향 출력
-		Serial.print("Counter: ");
-		Serial.println(counter);              // 카운팅 출력
-
-		//7seg에 출력
-		PrintTimeSeg(counter);
-
-	}
-
-	// 마지막 상태 변수 저장
-	lastStateCLK = currentStateCLK;
 }
--- ProMini_encoder_timer/read.md
+++ ProMini_encoder_timer/read.md
@@ -2,8 +2,8 @@
 
 자외선 노광기를 위한 타이머 구성
     [v] 로터리 인코더 {CLK:2, DT:3}. interupt 회로에 사용
-    []  4 digits 7 segments display
-        () 아두이노에서 나오는 핀과 드라이버 PCB는 분리해서 회로 구성한다.
+    [v]  4 digits 7 segments display
+        (x) 아두이노에서 나오는 핀과 드라이버 PCB는 분리해서 회로 구성한다.
     [] 릴레이 구성으로 램프를 구동하기 위한 회로가 필요하다.
 
 개발 일정 2023/8/10부터
@@ -15,6 +15,9 @@
             └─ 프로그램 시현 정도만 채택하였음
 
 PrintSeg(int arg)
-PrintTimeSeg(int nCounter)
-SetZero(void)
-SetInit(void)
+PrintTimeSeg(float fCounter)
+SetZero(void) // counter를 0으로 만드는 것이고
+SetInit(void) // 초기화된 counter(90)를 표시하는 것
+
+type definition이 잘못되어서 34초에서 로직에러를 자꾸만 내었음. 해결은 자리수를 늘리는 수밖에 없었고,  type casting을 엄밀하게 해야 되었음.
+8/20    [] 7 세그먼트 LED 프로그램 안착
