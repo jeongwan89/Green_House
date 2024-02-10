d0d0f28fe934662e9b7f55a03e9a74fc987ebdba
jeongwan89 <jeongwan89@mac.com>
2024-02-10 12:26:06 +0900

    calibration to zero

--- ESP01_MQTT_DHT22/ESP01_MQTT_DHT22.ino
+++ ESP01_MQTT_DHT22/ESP01_MQTT_DHT22.ino
@@ -23,7 +23,7 @@
   define의 정의를 고쳐서 컴파일하는 일을 컴파일 인자를 고쳐 컴파일한다고 하겠다.
   #define구문의 정의는 Esp01_DHT22_MQTT_01 ~ xx (100개)로 하고 각 define의 인자를 정의하는 것으로 한다.
 */
-#define Esp01_DHT22_MQTT_03
+#define Esp01_DHT22_MQTT_01
 
 #ifdef Esp01_DHT22_MQTT_01
 #define CLIENT_NAME   "Green_House_Temp_RH_Monitor_01"
@@ -31,7 +31,7 @@
 #define MQTT_PUB_HUM  "Sensor/GH1/Center/Hum"
 #define SENSOR_STATUS "Sensor/GH1/Center/Stat"
 #define TEMP_CAL  0
-#define HUM_CAL   2.3
+#define HUM_CAL   0
 #endif
 
 #ifdef Esp01_DHT22_MQTT_02
@@ -48,8 +48,8 @@
 #define MQTT_PUB_TEMP "Sensor/GH3/Center/Temp"
 #define MQTT_PUB_HUM  "Sensor/GH3/Center/Hum"
 #define SENSOR_STATUS "Sensor/GH3/Center/Stat"
-#define TEMP_CAL  -1.3
-#define HUM_CAL   4.4
+#define TEMP_CAL  0
+#define HUM_CAL   0
 #endif
 
 #ifdef Esp01_DHT22_MQTT_04
@@ -57,8 +57,8 @@
 #define MQTT_PUB_TEMP "Sensor/GH4/Center/Temp"
 #define MQTT_PUB_HUM  "Sensor/GH4/Center/Hum"
 #define SENSOR_STATUS "Sensor/GH4/Center/Stat"
-#define TEMP_CAL  0.1
-#define HUM_CAL   -1
+#define TEMP_CAL  0
+#define HUM_CAL  0
 #endif
 
 #ifdef Esp01_DHT22_MQTT_05
