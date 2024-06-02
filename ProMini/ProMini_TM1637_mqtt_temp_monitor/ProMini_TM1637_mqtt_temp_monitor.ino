3b0c25a7d7e438de35e9b8a3adb8f9e8f49d56db
jeongwan89 <jeongwan89@mac.com>
2024-06-02 21:45:03 +0900

    checked it

--- ProMini/ProMini_TM1637_mqtt_temp_monitor/ProMini_TM1637_mqtt_temp_monitor.ino
+++ ProMini/ProMini_TM1637_mqtt_temp_monitor/ProMini_TM1637_mqtt_temp_monitor.ino
@@ -1,10 +1,15 @@
 #include <stdio.h>
+const int LED = 13;
+
 void setup()
 {
-
+    pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
-
+    digitalWrite(LED, HIGH);
+    delay(50);
+    digitalWrite(LED, LOW);
+    delay(50);
 }
