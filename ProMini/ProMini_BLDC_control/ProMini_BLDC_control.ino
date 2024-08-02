bea4c229484d1509f24edc47fd9eac368733c0bb
jeongwan89 <jeongwan89@mac.com>
2024-08-02 22:56:34 +0900

    update BLCD

--- ProMini/ProMini_BLDC_control/ProMini_BLDC_control.ino
+++ ProMini/ProMini_BLDC_control/ProMini_BLDC_control.ino
@@ -2,23 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define debug
+
 #define VR A0
 #define PWM 3
+#ifdef debug
+#define PWM_LED 5
+#endif
 
-#define debug
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(VR, INPUT);
     pinMode(PWM, OUTPUT);
+#ifdef debug
+    pinMode(PWM_LED, OUTPUT);
     Serial.begin(115200);
+#endif
 }
 
 void loop()
 {
-    static int vrValue;
+    int vrValue;
+    int vrMapped;
     vrValue = analogRead(VR);
-#ifdef debuf
+    vrMapped = map(vrValue, 7, 1023, 0, 255);
+#ifdef debug
     Serial.println(vrValue);
+    analogWrite(PWM_LED, vrMapped);
 #endif
+    analogWrite(PWM, vrMapped);
 }
