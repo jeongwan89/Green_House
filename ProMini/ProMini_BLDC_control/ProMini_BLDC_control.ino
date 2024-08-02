71cca4446dba45217fea4efc1bb6604351ba38b6
jeongwan89 <jeongwan89@mac.com>
2024-08-02 21:45:21 +0900

    chg

--- ProMini/ProMini_BLDC_control/ProMini_BLDC_control.ino
+++ ProMini/ProMini_BLDC_control/ProMini_BLDC_control.ino
@@ -2,13 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define VR A0
+#define PWM 3
+
+#define debug
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
-
+    pinMode(VR, INPUT);
+    pinMode(PWM, OUTPUT);
+    Serial.begin(115200);
 }
 
 void loop()
 {
-    
+    static int vrValue;
+    vrValue = analogRead(VR);
+#ifdef debuf
+    Serial.println(vrValue);
+#endif
 }
