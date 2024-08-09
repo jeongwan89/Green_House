d97442ad22f9d67ffce811e584b0abdb394f6d24
jeongwan89 <jeongwan89@mac.com>
2024-08-09 20:04:20 +0900

    스위치 전부 분리

--- ProMini/ProMini_BLDC_control/ProMini_BLDC_control.ino
+++ ProMini/ProMini_BLDC_control/ProMini_BLDC_control.ino
@@ -7,16 +7,23 @@
 #define PWM_LED 5
 #endif
 
-
+// Pin 정의
 #define VR  A0
+#define VR_DURA A1
 #define PWM 3
 #define DIR 4
 #define DIR_SW  6
 #define FEED 7
-#define MODE 8
+#define ONE_STEP 8
+#define DIR_LED 9
+#define MODE_LED 10
+#define MODE_SELECT_SW 11
+// 상주 정의
 #define ONE_STEP_DURATION 1000
-#define MAX_SPEED 100   // UP TO 255
-bool motorDir = 0;
+#define MAX_SPEED 255   // UP TO 255
+#define DURA_MIN 1 // mode1에서 최소 지속시간
+#define DURA_MAX 30 // mode1에서 최대 지속시간
+bool motorDir = 0; 
 bool mode = 0;
 /**
  * loop()에서 방향을 바꿀 때(pin DIR 사용) 부드럽게 정지하고 
@@ -27,7 +34,7 @@ void changeDir(int initialPWM)
 {
     int currVrMapped = initialPWM;
     // decrement
-    for(int i=0; i < 256; i++)
+/*     for(int i=0; i < 256; i++)
     {
         if(currVrMapped == 0) break;
         currVrMapped -- ;
@@ -36,11 +43,12 @@ void changeDir(int initialPWM)
         analogWrite(PWM_LED, currVrMapped);
         #endif
         delay(20);
-    }
+    } */
     motorDir = !motorDir;
     digitalWrite(DIR, motorDir);
+    digitalWrite(DIR_LED, motorDir);
     // increment
-    for(int i=0; i < 256; i++)
+/*     for(int i=0; i < 256; i++)
     {
         if(currVrMapped == initialPWM) break;
         currVrMapped ++ ;
@@ -49,7 +57,7 @@ void changeDir(int initialPWM)
         analogWrite(PWM_LED, currVrMapped);
         #endif
         delay(40);
-    }
+    } */
 }
 
 /**
@@ -59,14 +67,12 @@ void changeDir(int initialPWM)
  */
 int modeCheck(void)
 {
-    static int lastDir=1, lastMode=1;
-    int currDir, currMode;
-    currDir = digitalRead(DIR_SW);
-    currMode = digitalRead(MODE);
+    static int lastMode=1;
+    static int flip = 0; 
+    int currMode;
+    currMode = digitalRead(MODE_SELECT_SW);
     delay(100);
-    if(lastDir == 0 && currDir ==  0 && lastMode == 0 && currMode == 0) 
-        // 방향키가 눌러졌다
-        // 모드키가 눌려졌다.
+    if(lastMode == 1 && currMode == 0) 
     {
         mode = !mode;
         // 두 조건으로 mode가 바뀌었음으로 그 전의 조건들을 초기화 한다.
@@ -76,19 +82,8 @@ int modeCheck(void)
         analogWrite(PWM_LED, 0);
         Serial.println("Now mode changed");
         #endif
-
-        // 계속 누르고 있을 때가 문제!
-        // 계속 누르고 있는지 확인하고 끝이 났을 때 정상으로 process진행
-        while(!(digitalRead(DIR_SW) && digitalRead(MODE)))
-        {
-            currDir = digitalRead(DIR_SW);
-            currMode = digitalRead(MODE);
-            #ifdef debug
-            Serial.println("Now exit modeCheck()");
-            #endif
-        }
+        digitalWrite(MODE_LED, mode);
     }
-    lastDir = currDir;
     lastMode = currMode;
     return mode;
 }
@@ -97,10 +92,10 @@ int modeCheck(void)
  * mode 1에서 필요한 함수.
  * @param: duration은 가속 지속 시간을 의미한다.
  */
-void moveOneStep(unsigned long duration)
+void moveOneStep(unsigned long duration, int currSpeed)
 {
     // 가속구간
-    for(int i = 0; i < MAX_SPEED; i++)
+    for(int i = 0; i < currSpeed; i++)
     {
         analogWrite(PWM, i);
         #ifdef debug
@@ -111,7 +106,7 @@ void moveOneStep(unsigned long duration)
     // 지속구간
     delay(duration);
     // 감속구간
-    for(int i = MAX_SPEED; i >= 0; i--)
+    for(int i = currSpeed; i >= 0; i--)
     {
         analogWrite(PWM, i);
         #ifdef debug
@@ -128,9 +123,13 @@ void setup()
     pinMode(VR, INPUT);
     pinMode(PWM, OUTPUT);
     pinMode(DIR, OUTPUT);
+    pinMode(DIR_LED, OUTPUT);
+    pinMode(MODE_LED, OUTPUT);
     pinMode(DIR_SW, INPUT_PULLUP);
     pinMode(FEED, INPUT_PULLUP);
-    pinMode(MODE, INPUT_PULLUP);
+    pinMode(ONE_STEP, INPUT_PULLUP);
+    pinMode(MODE_SELECT_SW, INPUT_PULLUP);
+    pinMode(VR_DURA, INPUT);
 
 #ifdef debug
     pinMode(PWM_LED, OUTPUT);
@@ -142,56 +141,71 @@ void setup()
 void loop()
 {
     static int lastStateSW = 1;
-    static int lastModeSW = 1;
     static int lastMode = 0;
-    int stateSW = 1;
-    int modeSW = 1;
+    static int keepDir = 0;
+    static int lastOneStep = 1;
+    int stateSW = 1; // 방향
+    int modeSW = 1;  // 모드
     int currMode;
+    int currOneStep;
     int vrValue;
     int vrMapped;
+    int duraMode1;
+    int duraMode1Mapped;
     // mode 0 : 연속적으로 움직이는 상태. 방향전환 키를 누르면 회전을 역전
     // mode 1 : mode를 한번 누르면 일정한 시간만큼 가는 상태. 방향 전환키는 회전을 역전
     currMode = modeCheck();
+    vrValue = analogRead(VR);
+    vrMapped = map(vrValue, 7, 1023, 0, MAX_SPEED);
+    vrMapped > 0 ? : vrMapped = 0;
 
     if(currMode == 0) {
-        vrValue = analogRead(VR);
-        vrMapped = map(vrValue, 7, 1023, 0, 255);
-        vrMapped > 0 ? : vrMapped = 0;
+        // mode 0 :
+        motorDir = keepDir;
 
     #ifdef debug
+        Serial.print("vrValue : ");
         Serial.println(vrValue);
         analogWrite(PWM_LED, vrMapped);
     #endif
         analogWrite(PWM, vrMapped);
         stateSW = digitalRead(DIR_SW);
-        if(lastStateSW != stateSW)
+        if(lastStateSW ==1 && stateSW ==0)
         {
-            delay(100);
-            if(stateSW == LOW) 
-            {
-                changeDir(vrMapped);
-            }
-            lastStateSW = stateSW;
+            changeDir(vrMapped);
         }
+        keepDir = motorDir;
     } 
-    else // modeCheck가 0이 아니라는 것은 모드가 1로 바뀌었다는 뜻
+    // 조건부에 들어가면서 mode 1:이 처음으로 행해지는 시기의 조건을 정의한다.
+    // 건들여졌던 motorDir을 연속적으로 사용하여야 하겠다.
+    else // mode 1 : 계속
     {
         // MODE(define에 정의됨)가 눌러지면(LOW) 일정기간 움직임. 
         // 방향은 DIR(define에 정의됨)에 따라 움직인다. mode 0에서 정의됨 값을
         // 그대로 사용한다.
-        modeSW = digitalRead(MODE);
         stateSW = digitalRead(DIR_SW);
-        if(lastStateSW == 0 && stateSW == 0){
+        duraMode1 = analogRead(VR_DURA);
+        duraMode1Mapped = map(duraMode1, 10, 1023, DURA_MIN, DURA_MAX);
+        duraMode1Mapped > DURA_MIN ? : duraMode1Mapped = DURA_MIN;
+        #ifdef debug
+        Serial.print("duraMode1 : ");
+        Serial.println(duraMode1);
+        #endif
+        if(lastStateSW == 1 && stateSW == 0){
             motorDir = !motorDir;
             digitalWrite(DIR, motorDir);
+            digitalWrite(DIR_LED, motorDir);
         }
+        currOneStep = digitalRead(ONE_STEP);
         delay(100);
-        if(lastModeSW == 0 && modeSW == 0)
+        if(lastOneStep == 1 && currOneStep == 0)
         {
-            moveOneStep(ONE_STEP_DURATION);
+            moveOneStep(duraMode1Mapped * 1000, vrMapped);
         }
-        lastModeSW = modeSW;
         lastStateSW = stateSW;
+        keepDir = motorDir;
+        lastOneStep = currOneStep;
     }
     lastMode = currMode;
+    lastStateSW = stateSW;
 }
